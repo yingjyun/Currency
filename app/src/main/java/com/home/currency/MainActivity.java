326239d9fcc1419d8e031ff948d4a2170bfefcba
yingjyun <43268589+yingjyun@users.noreply.github.com>
2019-02-25 20:58:23 +0800

    hw3

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -28,18 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         if (s_NTD.equals("")) {
             new AlertDialog.Builder(this)
-                    .setTitle("Problem")
-                    .setMessage("Please enter ou NTD amount")
-                    .setPositiveButton("OK", null)
+                    .setTitle(R.string.problem)
+                    .setMessage(R.string.please_enter_ntd)
+                    .setPositiveButton(R.string.ok, null)
                     .show();
         } else {
             float NTD = Float.parseFloat(s_NTD);
             float USD = NTD / 30.9f;
-            Log.d("MainActivity", "USD is " + USD);
+            Log.d("MainActivity", getString(R.string.usd_is) + USD);
             new AlertDialog.Builder(this)
-                    .setTitle("Result")
-                    .setMessage("USD is " + USD)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    .setTitle(R.string.result)
+                    .setMessage(getString(R.string.usd_is) + USD)
+                    .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             edntd.setText("");
